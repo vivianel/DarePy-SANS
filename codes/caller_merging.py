28ab809601b8a8b0828171c86d26d760daa2be13
Viviane Lutz-Bueno <viviane.lutz-bueno@psi.ch>
2023-12-15 13:13:17 +0100

    Adding error bar to the saved merged files.

--- codes/caller_merging.py
+++ codes/caller_merging.py
@@ -7,7 +7,7 @@ Created on Thu Aug 17 16:33:06 2023
 
 import post_processing as pp
 
-path_dir_an = 'C:/Users/lutzbueno_v/Documents/Analysis/data/Connor/SANS_2022_2581/DarePy-SANS/analysis/'
+path_dir_an = 'C:/Users/lutzbueno_v/Documents/Analysis/data/2022_0915_Combet/DarePy-SANS/analysis/'
 
 
 
@@ -16,18 +16,20 @@ path_dir_an = 'C:/Users/lutzbueno_v/Documents/Analysis/data/Connor/SANS_2022_258
 merged_files = pp.plot_all_data(path_dir_an)
 
 # %% STEP 2: REMOVE POINTS AND MERGE
-skip_start = [2,15 , 10]
-skip_end = [10, 10, 1]
+skip_start = {'2':1,'1':5 ,'0':5}
+skip_end = {'2':40,'1':5 ,'0':2}
 
-interp_type = 'log' # 'log' or 'linear' or 'none' for no interpolation
 
-pp.merging_data(path_dir_an, merged_files, skip_start, skip_end, interp_type)
+interp_type = 'none' # 'log' or 'linear' or 'none' for no interpolation
+interp_points = 100
+
+pp.merging_data(path_dir_an, merged_files, skip_start, skip_end, interp_type, interp_points)
 
 
 
 # %% FIT THE POROD LINE AND REMOVE INCOHERENT
 
 # define the range of the inoherent part to fit
-Last_points_fit = 80
+Last_points_fit = 30
 
 pp.subtract_incoherent(path_dir_an, Last_points_fit)
