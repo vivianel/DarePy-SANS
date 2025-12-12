df5398caaa98c138a9e5d36a68d08fa3eab0d8ae
Artur Glavic <artur.glavic@psi.ch>
2025-12-12 10:16:09 +0100

    Create basic facility for command line interface with different actions

--- darepy/__main__.py
+++ darepy/__main__.py
@@ -1,13 +1,36 @@
 """
 Executabel for the darepy package. Used to dispatch calls from the command line.
 """
-from .config.experiment import Experiment
+import importlib
+import sys
+from . import actions
+from .config import base
 
-def create_default_config():
-    # create the default config file
-    Experiment().save_config_file()
+
+def print_help():
+    print("Usage: darepy ACTION [-h] [...]\n    Available actions:")
+    for action in actions.actions:
+        module = importlib.import_module(f'darepy.actions.{action.name}', package='darepy.actions')
+        print(f"        {action.name} - {module.__doc__.strip().splitlines()[0]}")
+
+def add_global_options(parser):
+    parser.add_argument('-c', '--config', dest='config_file', default=base.ConfigObject.config_file,
+                        help='Name of config file to be used')
+
+def eval_global_options(arguments):
+    base.ConfigObject.config_file = arguments.config_file
+
+def main():
+    if len(sys.argv) < 2:
+        print_help()
+    else:
+        action = sys.argv[1]
+        module = importlib.import_module(f'darepy.actions.{action}', package='darepy.actions')
+        parser = module.Action.get_parser()
+        add_global_options(parser)
+        arguments = parser.parse_args(sys.argv[2:])
+        runner = module.Action(arguments)
+        runner.run()
 
 if __name__ == "__main__":
-    import sys
-    if '-c' in sys.argv:
-        create_default_config()
+    main()
